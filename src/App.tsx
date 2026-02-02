2a918812edceb1e660b0b4c5c4e2603ebea907ba
EoinClancy92 <eclancy54@gmail.com>
2026-02-02 19:28:28 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import LoginButton from './components/LoginButton.tsx';
-import LogoutButton from './components/logoutButton.tsx';
+import LogoutButton from './components/LogoutButton.tsx';
 import Profile from './components/Profile.tsx';
 import { useAuth0 } from '@auth0/auth0-react';
 
